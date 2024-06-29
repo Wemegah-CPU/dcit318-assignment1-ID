279934cbcc8fd921e29bbe8cbcd28b699f5b7be7
Wemegah <swemegah001@st.ug.edu.gh>
2024-06-29 19:22:12 +0100

    price calculator

---
+++ price_calculator.cs
@@ -0,0 +1,23 @@
+using System;
+
+class TicketPriceCalculator
+{
+    static void Main()
+    {
+        Console.Write("Enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+
+        int ticketPrice;
+
+        if (age <= 12 || age >= 65)
+        {
+            ticketPrice = 7;
+        }
+        else
+        {
+            ticketPrice = 10;
+        }
+
+        Console.WriteLine($"The ticket price is: GHC{ticketPrice}");
+    }
+}
